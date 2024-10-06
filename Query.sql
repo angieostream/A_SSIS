bbebaf307c5fbd4bae46c7776540abc656089576
Angie <angelyn.jimeno@g.msuiit.edu.ph>
2024-10-06 22:55:41 +0800

    Final (real)

--- Query.sql
+++ Query.sql
@@ -1,29 +1,30 @@
-DROP DATABASE `webssis` ;
+DROP DATABASE IF EXISTS `webssis`;
 
-CREATE  DATABASE `webssis` ;
-USE `webssis` ;
-CREATE TABLE `college` (
-  `code` VARCHAR(10) NOT NULL,  
-  `name` VARCHAR(255) NOT NULL, 
+CREATE DATABASE `webssis`;
+USE `webssis`;
+
+CREATE TABLE `colleges` (
+  `code` VARCHAR(10) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`code`)
 );
 
-CREATE TABLE `program` (
-  `code` VARCHAR(10) NOT NULL, 
-  `name` VARCHAR(255) NOT NULL, 
-  `college` VARCHAR(10),        
-  PRIMARY KEY (`code`),         
-  FOREIGN KEY (`college`) REFERENCES `college`(`code`)
+CREATE TABLE `programs` (
+  `code` VARCHAR(10) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `college` VARCHAR(10),
+  `college_code` VARCHAR(10),  -- New column for college_code
+  PRIMARY KEY (`code`),
+  FOREIGN KEY (`college`) REFERENCES `colleges`(`code`)
 );
 
-CREATE TABLE `student` (
-  `id` VARCHAR(9) NOT NULL,   
+CREATE TABLE `students` (
+  `id` VARCHAR(9) NOT NULL,
   `firstname` VARCHAR(255) NOT NULL,
   `lastname` VARCHAR(255) NOT NULL,
-  `course` VARCHAR(10),       
-  `year` INT(1) NOT NULL,     
-  `gender` ENUM('M', 'F', 'O') NOT NULL, 
-  PRIMARY KEY (`id`),       
-  FOREIGN KEY (`course`) REFERENCES `program`(`code`)
+  `course` VARCHAR(10),
+  `year` INT NOT NULL,
+  `gender` ENUM('Male', 'Female', 'Other') NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`course`) REFERENCES `programs`(`code`)
 );
-
